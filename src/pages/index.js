0812f4876aa172ae869fe2fc3093fee75f2d987d
wolfwar <wolfwar@outlook.com>
2019-07-25 16:26:43 +0200

    added bootstrap and reactstrap and plugins, creeated navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Home" keywords={["gatsby", "application", "react"]} />
     <h1>Home page</h1>
   </Layout>
 )
